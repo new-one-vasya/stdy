67fddbca63812f870f956364fbb10d0ac2324500
Sergey Sergunin <new.one.vasya.pupkin@gmail.com>
2022-11-28 02:32:47 +0300

    Работает, но опять не как надо

--- lesson5/l5-client/src/main/java/nw/one/vasya/lesson5/Client.java
+++ lesson5/l5-client/src/main/java/nw/one/vasya/lesson5/Client.java
@@ -4,30 +4,49 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Client {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Client.class);
-    private static boolean NEED_SAND_STOP = true;
+    private static final int BUFFER_SIZE = 1024;
+    static int port = 11;
+    private static List<String> myList = new ArrayList<String>(
+            Arrays.asList("Tyrion Lannister",
+                    "Cersei Lannister",
+                    "Daenerys Targaryen",
+                    "Jon Snow",
+                    "Sansa Stark",
+                    "Arya Stark",
+                    "Jaime Lannister",
+                    "Jorah Mormont",
+                    "Lord Varys"));
 
     public static void main(String[] args) throws IOException {
+        logger("Starting MyDatagramClientExample...");
 
-        if (args.length != 1){
-            LOGGER.warn("Где хост?");
-            return;
+        InetAddress hostIP = InetAddress.getLocalHost();
+        InetSocketAddress myAddress =
+                new InetSocketAddress(hostIP, port);
+        DatagramChannel datagramChannel = DatagramChannel.open();
+        datagramChannel.configureBlocking(false); //
+        datagramChannel.bind(null);
+
+        ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+        for (String cast: myList) {
+            logger("sending..: " + cast);
+            buffer.put(cast.getBytes());
+            buffer.flip();
+            datagramChannel.send(buffer, myAddress);
+            buffer.clear();
         }
+    }
 
-        DatagramChannel datagramChannel = DatagramChannel.open().bind(null);
-        datagramChannel.configureBlocking(false);
-
-        String msg = "hello";
-        InetSocketAddress serverAddress = new InetSocketAddress("localhost", 9999);
-
-        ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
-        datagramChannel.send(buffer, serverAddress);
-
+    public static void logger(String msg) {
+        System.out.println(msg);
     }
 }
--- lesson5/l5-server/src/main/java/nw/one/vasya/lesson5/Server.java
+++ lesson5/l5-server/src/main/java/nw/one/vasya/lesson5/Server.java
@@ -4,33 +4,47 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
+import java.util.Objects;
 
 public class Server {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
+    private static final int BUFFER_SIZE = 1024;
+    static int port = 11;
 
     public static void main(String[] args) throws IOException {
-        int count = 0;
-        DatagramChannel datagramChannel = DatagramChannel.open().bind(new InetSocketAddress("localhost", 9999));
-        datagramChannel.configureBlocking(false);
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        logger("Starting MyDatagramServerExample...");
+        InetAddress hostIP = InetAddress.getLocalHost();
+        InetSocketAddress address = new InetSocketAddress(hostIP, port);
+        DatagramChannel datagramChannel = DatagramChannel.open();
+        datagramChannel.configureBlocking(false); //
+        DatagramSocket datagramSocket = datagramChannel.socket();
+        datagramSocket.bind(address);
 
-        SocketAddress remoteAdd = null;
-        while(remoteAdd == null) {
-            count++;
-            remoteAdd = datagramChannel.receive(buffer);
+        SocketAddress addr = null;
+        int a = 0;
+
+        // Allocate a byte buffer
+        ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+        while (true) {
+            do {
+                addr = datagramChannel.receive(buffer);
+            } while (addr == null);
+            buffer.flip();
+            System.out.print("\nFrom " + addr + " Data...: ");
+//            while (buffer.hasRemaining()) {
+                System.out.write(buffer.get());
+//            }
+            buffer.clear();
         }
+    }
 
-        buffer.flip();
-        byte[] bytes = new byte[buffer.remaining()];
-        buffer.get(bytes);
-        String msg = new String(bytes);
-        System.out.println("Client at #" + remoteAdd + "  sent: " + msg);
-        System.out.println("Count # " + count);
-
+    public static void logger(String msg) {
+        System.out.println(msg);
     }
 }
